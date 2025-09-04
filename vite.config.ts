86fcf5f4c535e36804c5d3cdbdf9d5b77b607dab
Admin <104626801+deepscouting-admin@users.noreply.github.com>
2025-09-04 11:40:35 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   // Configuration pour GitHub Pages
-  base: mode === 'production' ? '/stat12-coach-insights/' : '/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' &&
